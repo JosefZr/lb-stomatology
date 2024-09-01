24e738e2e4db081eab10b5597296708f43a1a853
zeraibiyoucef <zeraibiyoucef1@gmail.com>
2024-09-01 03:26:30 +0100

    adding the favicon to the layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,6 +10,7 @@ export default async function RootLayout({ children }) {
     <html lang='en' className="scroll-smooth relative" suppressHydrationWarning>
       <head>
         <title>BENYAHIA FAMILY</title>
+        <link rel="icon" href="./favicon.ico" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Bienvenue sur le site de Light Stomatology. Nous offrons des soins dentaires complets et de qualité pour toute la famille. Découvrez nos services, prenez rendez-vous en ligne, et restez informé des dernières actualités dentaires." />
